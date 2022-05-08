43887913d89c7afcc8f436ec0999f8f4f1250039
Nataliia Kovaliv <nataliiakovaliv95@gmail.com>
2022-05-08 20:17:12 +0300

    feat: implement saving user times played data

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -19,21 +19,20 @@ export class QuizService {
   private _state$!: BehaviorSubject<QuizState>;
   private _quizzes = QUIZZES;
   private _userQuizzes!: QuizData[];
+  private _userTimesPlayedData!: { [key: string]: number };
   private _randomQuizUrl: string = 'https://opentdb.com/api.php?amount=10';
   private _quizCategoriesUrl: string = 'https://opentdb.com/api_category.php';
   private _questionCountUrl: string = 'https://opentdb.com/api_count.php?category=';
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
     this._userQuizzes = JSON.parse(localStorage.getItem('userQuizzes')!) || [];
+    this._userTimesPlayedData = JSON.parse(localStorage.getItem('userTimesPlayedData')!) || {};
     this._quizzes = [...this._quizzes, ...this._userQuizzes];
+    this._quizzes.map(quiz => quiz.timesPlayed = this._userTimesPlayedData[quiz.id] || 0)
   }
 
   public getQuizzes(): QuizData[] {
     return this._quizzes;
-  }
-
-  public getRandomQuiz(): Observable<QuizItem[]> {
-    return this.http.get(this._randomQuizUrl).pipe(map((response: any) => response.results));
   }
 
   public getState(): BehaviorSubject<QuizState> {
@@ -46,6 +45,10 @@ export class QuizService {
 
   public setState(stateData: QuizState): void {
     this._state$ = new BehaviorSubject<QuizState>(stateData);
+  }
+  
+  public getRandomQuiz(): Observable<QuizItem[]> {
+    return this.http.get(this._randomQuizUrl).pipe(map((response: any) => response.results));
   }
 
   public getQuiz(quizRequestData: CreateQuizData): Observable<QuizItem[]> {
@@ -105,7 +108,7 @@ export class QuizService {
       }
       allAnswers.push(answers);
     }
-    return allAnswers
+    return allAnswers;
   }
 
   public searchQuiz(text: string): QuizData[] {
@@ -150,6 +153,10 @@ export class QuizService {
     localStorage.setItem('userQuizzes', JSON.stringify([...this._userQuizzes, newQuiz]));
   }
 
+  private _saveUserTimesPlayedData(): void {
+    localStorage.setItem('userTimesPlayedData', JSON.stringify(this._userTimesPlayedData));
+  }
+
   private _setPartialState(partialState: Partial<QuizState>): void {
     this._state$.next({ ...this.getStateValue(), ...partialState });
   }
@@ -158,16 +165,18 @@ export class QuizService {
     let correctAnswersCount = 0;
     for (let i = 0; i < userAnswers.length; i++) {
       if (userAnswers[i] === currentQuiz[i].correct_answer) {
-        correctAnswersCount++
+        correctAnswersCount++;
       }
     }
-    return correctAnswersCount
+    return correctAnswersCount;
   }
 
   private _changeTimesPlayedData(quiz: QuizItem[]): void {
     this._quizzes.map(quizData => {
       if (quizData.quiz.every(el => quiz.includes(el))) {
-        quizData.timesPlayed++
+        quizData.timesPlayed++;
+        this._userTimesPlayedData[quizData.id] = quizData.timesPlayed;
+        this._saveUserTimesPlayedData();
       }
     });
   }
