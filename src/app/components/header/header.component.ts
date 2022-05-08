e8cf4b58538cca577106d3505963bc54cb99ad91
Nataliia Kovaliv <nataliiakovaliv95@gmail.com>
2022-05-08 22:08:54 +0300

    feat: add card hover animation, add scroll anchor

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  constructor(private viewportScroller: ViewportScroller) { }
+
+  public toTop(): void {
+    this.viewportScroller.scrollToPosition([0, 0]);
+  }
+}
