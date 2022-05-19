56817623fc5f6a9e569f31a3a29eceb57927f409
Nataliia Kovaliv <nataliiakovaliv95@gmail.com>
2022-05-19 16:44:27 +0300

    feat: implement authorization guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,16 @@ import { QuizPageComponent } from '@components/quiz-page/quiz-page.component';
 import { AuthorizationPageComponent } from '@components/authorization-page/authorization-page.component';
 import { LeaveQuizGuard } from '@guards/leave-quiz.guard';
 import { StatisticPageComponent } from '@components/statistic-page/statistic-page.component';
-import { AuthorizationGuard } from '@guards/authorization.guard';
+import { IsLoggedInGuard } from '@guards/is-logged-in.guard';
+import { IsLoggedOutGuard } from '@guards/is-logged-out.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'quiz', component: QuizPageComponent, canDeactivate: [LeaveQuizGuard] },
-  { path: 'registration', component: AuthorizationPageComponent },
-  { path: 'authorization', component: AuthorizationPageComponent },
-  { path: 'statistic', component: StatisticPageComponent, canActivate: [AuthorizationGuard] },
+  { path: 'registration', component: AuthorizationPageComponent, canActivate: [IsLoggedOutGuard] },
+  { path: 'authorization', component: AuthorizationPageComponent, canActivate: [IsLoggedOutGuard] },
+  { path: 'statistic', component: StatisticPageComponent, canActivate: [IsLoggedInGuard] },
 ];
 
 @NgModule({
